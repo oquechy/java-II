ecb54823c9eb002da635cf1225bed9713ad1dd6e
Elizaveta Vasilenko <vasilliza@gmail.com>
2018-02-16 22:23:19 +0300

    make build.sh more informative

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 find . -maxdepth 1 -mindepth 1 -type d | while read -r hw ; do
     if [[ "$hw" == ./hw* ]] ; then
         echo "$hw"
-        cd "$hw" && gradle check || exit 1
+        cd "$hw" || { echo "cd failed" ; exit 1 ; } 
+        gradle check || { echo "check failed" ; exit 1 ; }
     fi
 done
 
