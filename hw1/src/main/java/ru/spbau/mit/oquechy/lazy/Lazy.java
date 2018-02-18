5f6d71e3276147725e813c52507fd299dede3c26
Elizaveta Vasilenko <vasilliza@gmail.com>
2018-02-18 18:39:01 +0300

    comments

--- hw1/src/main/java/ru/spbau/mit/oquechy/lazy/Lazy.java
+++ hw1/src/main/java/ru/spbau/mit/oquechy/lazy/Lazy.java
@@ -1,5 +1,10 @@
 package ru.spbau.mit.oquechy.lazy;
 
+/**
+ * Interface for lazy computations. Result of {@code get} should be
+ * computed once, no matter how many times it was called.
+ * @param <T> type of the result of the computation.
+ */
 public interface Lazy<T> {
     T get();
 }
--- hw1/src/main/java/ru/spbau/mit/oquechy/lazy/LazyFactory.java
+++ hw1/src/main/java/ru/spbau/mit/oquechy/lazy/LazyFactory.java
@@ -4,10 +4,25 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Supplier;
 
+/**
+ * Produces two implementations of {@link Lazy}.
+ *
+ * SingleThreadLazy guarantees that computation will be computed
+ * just once if only one thread can call its {@code get} method.
+ *
+ * MultiThreadLazy guarantees that computation will be computed
+ * just once even if several threads can call its {@code get} method.
+ *
+ */
 public class LazyFactory {
 
     private static final Object mutex = new Object();
 
+    /**
+     * Returns {@link Lazy} for working in single thread.
+     * @param sup computation to be called
+     * @param <T> type of result of the computation
+     */
     public static <T> Lazy<T> createSingleThreadLazy(Supplier<T> sup) {
         return new Lazy<T>() {
             private T result;
@@ -26,6 +41,11 @@ public class LazyFactory {
         };
     }
 
+    /**
+     * Returns {@link Lazy} for working in several threads.
+     * @param sup computation to be called
+     * @param <T> type of result of the computation
+     */
     public static <T> Lazy<T> createMultiThreadLazy(Supplier<T> sup) {
         return new Lazy<T>() {
             private T result;
