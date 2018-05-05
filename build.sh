91cde0bbb04205876a7380bc7be11ef73fd2ff46
Elizaveta Vasilenko <vasilliza@gmail.com>
2018-05-05 13:03:27 +0300

    build.sh using gradlew

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 for hw in $(find . -maxdepth 1 -mindepth 1 -type d) ; do
     if [ -f "$hw/build.gradle" ] ; then
         echo "processing $hw"
-        cd "$hw" && gradle check && cd .. || { echo "check failed" ; exit 1 ; }
+        cd "$hw" && ./gradlew check -i && cd .. || { echo "check failed" ; exit 1 ; }
         echo "finishing $hw"
     fi
 done
