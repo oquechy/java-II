78da97fef55ce32acaddb4799604cae360989a03
Elizaveta Vasilenko <vasilliza@gmail.com>
2018-05-07 13:22:44 +0300

    build.sh update gradle version

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ for hw in $(find . -maxdepth 1 -mindepth 1 -type d) ; do
     if [ -f "$hw/build.gradle" ] ; then
         echo "processing $hw"
         cd "$hw"                              || { echo "cd $hw failed" ; exit 1 ; }
-        gradle wrapper --gradle-version 4.5.1 || { echo "gradle wrapper update failed" ; exit 1 ; }
+        gradle wrapper --gradle-version 4.6   || { echo "gradle wrapper update failed" ; exit 1 ; }
         ./gradlew check -i                    || { echo "check failed" ; exit 1 ; }
         cd ..                                 || { echo "cd .. failed" ; exit 1 ; }
         echo "finishing $hw"
